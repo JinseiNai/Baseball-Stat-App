cc8dd9911cea2e3decfab3ff938fefd48d3a776e
Jinsei <jinseinai@gmail.com>
2019-10-23 22:55:43 -0700

    set up env file

--- models/seeds.sql
+++ models/seeds.sql
@@ -11,7 +11,12 @@ INSERT INTO players ( team_id, first_name, last_name, jersey_number, AB, H, `1B`
 VALUES ( 1, "Calvin", "Wang", 25, 29, 11, 9, 2, 0, 0, 10, 6, 8, 0.379, 0.538, 0.448, 10 );
 INSERT INTO players ( team_id, first_name, last_name, jersey_number, AB, H, `1B`, `2B`, `3B`, HR, BB, RBI, R, average, OBP, SLG, K )
 VALUES ( 1, "Ryan", "Tate", 13, 29, 10, 9, 1, 0, 0, 8, 8, 6, 0.344, 0.486, 0.344, 7 );
+INSERT INTO players ( team_id, first_name, last_name, jersey_number, AB, H, `1B`, `2B`, `3B`, HR, BB, RBI, R, average, OBP, SLG, K )
+VALUES ( 11, "Kevin", "Tsung", 27, 39, 25, 18, 6, 1, 0, 8, 17, 24, 0.641, 0.702, 0.846, 4 );
 
 SELECT * FROM teams;
 SELECT * FROM players;
+SELECT * FROM players WHERE team_id = "11";
+SELECT first_name FROM players WHERE team_id = "1";
+
 
